2324827da78ccf0ccbfa74216060c17a98b08b1e
Armaan <armaanmarya2004@gmail.com>
2024-02-03 22:03:42 +0530

    FCFS

--- main.cpp
+++ main.cpp
@@ -1,24 +1,56 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-class nopriority{
+
+class nopriority {
+    //Since No Priority is there RT = WT
 public:
-    int A[100][4];
-    int bt,wt,tat,n;
-    nopriority(){
-        cout<<"Enter number of Processes: "<<endl;
-        cin>>n;
-        cout<<"Enter Burst Times: "<<endl;
-        for(int i=0;i<n;i++){
-            cout<<"Enter time of P"<<i+1<<": ";
-            cin>>A[i][1];
-            A[i][0]=i+1;
+    int n;
+    vector<int> p;
+    vector<int> wt;
+    vector<int> ta;
+
+    nopriority() {
+        cout << "Enter Number of Processes: " << endl;
+        cin >> n;
+
+        p.resize(n);
+        wt.resize(n, 0);
+        ta.resize(n, 0);
+
+        cout << "Enter Process Burst Times:" << endl;
+        for (int i = 0; i < n; i++)
+            cin >> p[i];
+
+    }
+
+
+    void FCFSdisplay() {
+
+        for (int i = 1; i < n; i++) {
+            wt[i] = p[i - 1] + wt[i - 1];
         }
-        for(int i=0;i<n;i++)
-            cout<<A[i][0]<<"  "<<A[i][1]<<endl;
+
+        for (int i = 0; i < n; i++) {
+            ta[i] = p[i] + wt[i];
+        }
+
+        cout << "Waiting Time     = ";
+        for (int i = 0; i < n; i++) {
+            cout << wt[i] << " ";
+        }
+        cout << endl;
+        cout << "Turn Around Time = ";
+        for (int i = 0; i < n; i++) {
+            cout << ta[i] << " ";
+        }
     }
 };
-int main(){
+
+int main() {
     nopriority obj;
+    obj.FCFSdisplay();
 
     return 0;
 }
+//NUM
